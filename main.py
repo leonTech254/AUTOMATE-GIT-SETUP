120fbad656f3ed15e585e1ce3918170ed3f0e689
leontech <martinleontech23@gmail.com>
2022-12-26 23:14:46 +0300

    -update

--- main.py
+++ main.py
@@ -23,10 +23,19 @@ class gitAutomate:
     # Adding your SSH key to the ssh-agent
 
     def Key(self, email):
-        subprocess.Popen(["ssh-keygen", "-t", "ed25519", "-C",
-                         f"{email}"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        process = subprocess.Popen(["ssh-keygen", "-t", "ed25519", "-C",
+                                    f"{email}"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        file_name = input("Enter the file name for the key: ")
+        file_name = file_name.encode()
+        output, err = process.communicate(file_name)
+        output, err = process.communicate(file_name)
 
-    def LocateKey():
+        subprocess.run(["eval", "$(ssh-agent -s)"])
+        subprocess.run(["ssh-add", "~/.ssh/id_ed25519"])
+        self.LocateKey()
+        # print(output.decode())
+
+    def LocateKey(self):
         # subprocess.run(["cat", "~/.ssh/id_ed25519.pub"])
         AvNet = os.popen("cat ~/.ssh/id_ed25519.pub").read()
         print(AvNet)
